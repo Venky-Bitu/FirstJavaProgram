3980c7765d5954fa7da0b7a4f73a47beaab0bed8
venky-bitu <venkatraman.patra@gmail.com>
2019-06-13 21:47:34 -0700

    Second Java Program

---
+++ SecondJavaProgram/src/secondjavaprogram/HelloPune.java
@@ -0,0 +1,13 @@
+package secondjavaprogram;
+
+public class HelloPune {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Pune \n");
+		System.out.println("I am Good \n");
+
+	}
+
+}
